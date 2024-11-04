fcecd1f42155f38d0e9c57d074b14196b1ce0534
CatherineAmir <catherine.amir20@gmail.com>
2024-11-04 15:16:23 +0200

    remove app_one

--- app_one/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import car_rent
-from . import car
-from . import driver
-from . import todo_task
--- app_one/models/faq_question.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-
-class FagQuestion(models.Model):
-	_name = 'faq.question'
-	_description = 'FAQ Question'
-	question = fields.Text(string='Question')
-	answer = fields.Text(string='Answer')
-	number_of_calls = fields.Integer(string='Number of Calls')
